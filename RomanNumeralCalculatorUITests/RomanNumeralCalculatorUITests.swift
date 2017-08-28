a18af94c5ee4f56c79f5e2141f40e4e8b6dac8a9
Andrew Dementiev <bigdigdgs@gmail.com>
2017-08-28 23:42:53 +0800

    My commit message

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -2,8 +2,8 @@
 //  RomanNumeralCalculatorUITests.swift
 //  RomanNumeralCalculatorUITests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by Andrew Dementiev on 28.08.17.
+//  Copyright © 2017 buddybuild. All rights reserved.
 //
 
 import XCTest
